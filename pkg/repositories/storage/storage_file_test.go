d359c775298927bb11c74b5119b90cbbb91e33c2
Vadim Sokoltsov <vforvad@gmail.com>
2020-11-30 00:25:22 +0300

    Add coverage file

--- pkg/repositories/storage/storage_file_test.go
+++ pkg/repositories/storage/storage_file_test.go
@@ -18,3 +18,33 @@ func TestStorageCreateFile(t *testing.T) {
 		log.Fatal(deleteErr)
 	}
 }
+
+func TestStorageOpenFile(t *testing.T) {
+	storage := NewFileStorage()
+	_, err := storage.CreateFile("./test")
+	if err != nil {
+		t.Errorf("Error of creating the file: %s", err)
+	}
+
+	_, oErr := storage.OpenFile("./test")
+	if err != nil {
+		t.Errorf("Error of opening fle: %s", oErr)
+	}
+	deleteErr := os.Remove("./test")
+	if deleteErr != nil {
+		log.Fatal(deleteErr)
+	}
+}
+
+func TestStorageResultPath(t *testing.T) {
+	storage := NewFileStorage()
+	var (
+		dir  = "/a/b/c/"
+		name = "test"
+		ext  = "txt"
+	)
+	path := storage.ResultPath(dir, name, ext)
+	if path != "/a/b/c/test.txt" {
+		t.Errorf("Generated file path does not match: %s", path)
+	}
+}

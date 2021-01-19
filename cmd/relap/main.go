2c42614a71539b2bfc7dab39e78e2fe6453792f0
Vadim Sokoltsov <vforvad@gmail.com>
2021-01-19 20:37:46 +0300

    Add comments to main.go file

--- cmd/relap/main.go
+++ cmd/relap/main.go
@@ -56,15 +56,23 @@ func main() {
 	)
 
 	flag.Parse()
+
+	// Define base project path
 	projectPath := getProjectPath("cmd")
 	resultPath := filepath.Join(projectPath, *resultsDir)
 	fs := storage.NewFileStorage(resultPath, *resultExt)
 
+	// Initialize HTML handler
 	htmlHandler := handler.NewHTML()
+
+	// Initialize worker for fetching pages
 	wrkr := worker.NewWorker(htmlHandler)
+
+	// Initializing read and write pool
 	readPool := pool.NewReadPool(*goNum, readWg, readJobs, readResults, wrkr)
 	writePool := pool.NewWritePool(*goNum, writeWg, writeJobs, writeResults)
 
+	// Opens initial file
 	absPath := filepath.Join(projectPath, *filePath)
 	file, fileErr := fs.OpenFile(absPath, os.O_RDONLY, 0644)
 	if fileErr != nil {
@@ -72,8 +80,11 @@ func main() {
 	}
 	defer file.Close()
 
+	// Run workers pool for read and write operations
 	readPool.StartWorkers()
 	writePool.StartWorkers()
+
+	// Define pipes for all operations
 	readerPipe := pipeline.NewReader(file, readResults, readWg, readJobs, errors)
 	writerPipe := pipeline.NewWriter(writeWg, writeJobs, writeResults, errors, fs)
 	combinerPipe := pipeline.NewCombiner()
@@ -83,6 +94,8 @@ func main() {
 		writerPipe,
 		combinerPipe,
 	}
+
+	// Execute pipeline
 	pipeline.ExecutePipeline(pipes...)
 	elapsed := time.Since(startTime)
 	log.Printf("Program has finished. It took %s", elapsed)
